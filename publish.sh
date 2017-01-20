3f84a18b7442807eaf3e1b5ad74368e39cb17c21
Simon Oulevay <simon.oulevay@gmail.com>
2017-01-20 11:34:06 +0100

    Commit generated documentation to separate repo

--- publish.sh
+++ publish.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
+
+# Get remote URL of documentation repo (either the `docs`)
+REPO_URL="$(git remote get-url docs 2>/dev/null)"
+if [ -z "$REPO_URL" ]; then
+  # Default documentation repo if no "docs" remote is configured
+  REPO_URL="git@github.com:MediaComem/comem-webdev-docs.git"
+fi
+
+# Stop if any command fails
 set -e
 
-REPO_URL="$(git remote get-url origin)"
 CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 CURRENT_COMMIT="$(git rev-parse --verify --short HEAD)"
 
@@ -18,6 +26,7 @@ function cleanup {
   rm -fr "$TMP_DIR"
 }
 
+# Clean up the temporary directory when the script exits (successfully or not)
 trap cleanup EXIT
 
 echo "$TMP_DIR"
@@ -36,7 +45,7 @@ echo "Fetching repository..."
 echo
 cd "$TMP_DIR"
 git init
-git remote add -t gh-pages origin "$REPO_URL"
+git remote add -t master origin "$REPO_URL"
 git fetch
 echo
 
@@ -45,10 +54,10 @@ echo
 echo "Committing & pushing changes..."
 echo
 cd "$TMP_DIR"
-git reset --soft origin/gh-pages
+git reset --soft origin/master
 git add --all .
 git commit -m "Generated content from ${CURRENT_BRANCH}@${CURRENT_COMMIT}"
-git push origin master:gh-pages
+git push origin master
 echo
 
 echo "Done!"
