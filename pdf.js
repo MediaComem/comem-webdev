f5cc87547e4c5f86ece1483c1f892557c4fd7324
Simon Oulevay <simon.oulevay@gmail.com>
2018-02-22 19:32:48 +0100

    Pdf generation tests (not working)

--- pdf.js
+++ pdf.js
@@ -1,5 +1,5 @@
+const fs = require('fs-extra');
 const _ = require('lodash');
-const fs = require('fs');
 const path = require('path');
 const pdf = require('html-pdf');
 
@@ -15,15 +15,13 @@ const pdf = require('html-pdf');
  * @param {String} options.height - The height of the screen (defaults to "768px")
  * @returns {Promise} A promise that will be resolved with the result of calling html-pdf on the source file
  */
-module.exports = function generatePdfFromSlides(src, dest, options) {
+module.exports = async function generatePdfFromSlides(src, dest, options) {
   if (!src || !src.match(/\.html$/)) {
     throw new Error('Source file ${src} is not an HTML file');
-  } else if (!fs.existsSync(src)) {
-    throw new Error(`Source file ${src} doesn't exist`);
   }
 
   // Read HTML from src file
-  const html = fs.readFileSync(src, 'utf-8');
+  const html = await readFile(src);
 
   // Base options (cannot be overriden)
   const pdfOptions = {
@@ -32,27 +30,55 @@ module.exports = function generatePdfFromSlides(src, dest, options) {
 
   // Options from environment variables
   const envOptions = {
+    renderDelay: parseEnvInt(process.env.PDF_RENDER_DELAY),
     width: process.env.PDF_WIDTH,
     height: process.env.PDF_HEIGHT
   };
 
   // Default options
   const defaultOptions = {
+    renderDelay: 10000,
     width: '1024px',
     height: '768px'
   };
 
   _.defaults(pdfOptions, options, envOptions, defaultOptions);
+  console.log(pdfOptions);
 
-  return new Promise((resolve, reject) => {
-    // Convert HTML to PDF
-    pdf.create(html, pdfOptions).toFile(dest, function(err, res){
-      if (err) {
-        return reject(err);
-      }
-
-      resolve(res);
-    });
-  });
+  return await convertHtmlToPdf(html, dest, pdfOptions);
 };
 
+async function readFile(filePath) {
+  try {
+    return await fs.readFile(filePath, 'utf8');
+  } catch(err) {
+    if (err.code === 'ENOENT') {
+      throw new Error(`File "${filePath}" does not exist`);
+    }
+
+    throw err;
+  }
+}
+
+function convertHtmlToPdf(htmlFilePath, destPath, pdfOptions) {
+  return new Promise((resolve, reject) => {
+    pdf
+      .create(htmlFilePath, pdfOptions)
+      .toFile(destPath, (err, res) => err ? reject(err) : resolve(res));
+  });
+}
+
+function parseEnvInt(name) {
+
+  const value = process.env[name];
+  if (!value) {
+    return;
+  }
+
+  const number = parseInt(value, 10);
+  if (!_.isInteger(number) || number < 0) {
+    throw new Error(`"$PDF_RENDER_DELAY" must be an integer greater than or equal to zero`);
+  }
+
+  return number;
+}
